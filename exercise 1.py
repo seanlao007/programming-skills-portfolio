e21cea51168e289415e33ea8c23330468952d364
seanlao007 <seanlao007@outlook.com>
2024-10-24 02:10:08 -0700

    Add files via upload

---
+++ exercise 1.py
@@ -0,0 +1,9 @@
+#Exercise 1 Coding is Cool
+
+# I made the variable and the input the appropriate words
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+# I connected all three words
+print(word1 + " " + word2 + " " + word3)
+
